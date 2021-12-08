e5c3c47b71693bc0d53f09896cb605b375482768
Yuri Carlos <yuryshadow@gmail.com>
2021-12-08 19:48:33 -0300

    Criando documentarios

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,34 @@
 package fundamentos;
 
+/** JAVADOC
+ * 
+ * @author Yuri Carlos <@gmail.com>
+ *
+ */
+
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
+		
+		/**
+		 * @param args
+		 */
+		
+		// Uma sentença de código terminar com ;
+		// Mais uma linha...
+		// Fim
 		System.out.println("Primeiro programa Parte #01 !!");
+		
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3
+		 */
+		
 		System.out.println("Primeiro programa Parte #02 !!");
+		
+		System.out.println("FIM"); // Aqui tb funciona !
 	}
 }
